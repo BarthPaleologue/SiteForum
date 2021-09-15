7ba0254e51033ea528c90700966452e735d2dd1a
Foguxed <61628044+Foguxed@users.noreply.github.com>
2021-09-16 00:35:16 +0200

    bouton anti scroll

--- scripts/script.js
+++ scripts/script.js
@@ -60,3 +60,48 @@ document.getElementById("membres").addEventListener("click", () => {
 });
 
 loadFileInto("./pages/accueil.html", contentContainer);
+
+topScrollButton = document.getElementById("gotoTopButton");
+const effect = new KeyframeEffect(
+  topScrollButton, // Element to animate
+  [ // Keyframes
+    { opacity: 0.5,
+    bottom:"-200px"
+    },
+    {opacity: 1,
+      bottom:"10px"
+    }
+  ],
+  {duration: 500, direction: "normal", easing: "linear", fill: "both"} // Keyframe settings
+);
+
+const topScrollButtonAnimation = new Animation(effect, Document.timeline);
+
+
+var animationStart = -1;
+var isAnimationUp = false;
+
+function checkScrollButton()
+{
+  var b = window.scrollY > 350;
+  if(b == isAnimationUp)
+  {
+    topScrollButtonAnimation.reverse();
+    isAnimationUp = !isAnimationUp;
+  }
+}
+
+
+function startAnimation(reverted)
+{
+  animationStart = htmlMediaElement.currentTime;
+}
+
+function goTop()
+{
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+document.addEventListener('scroll',checkScrollButton );
+console.log("logged");
