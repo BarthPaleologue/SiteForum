4ca2391e610585425b8af091d11ff480281b4363
Foguxed <61628044+Foguxed@users.noreply.github.com>
2021-10-04 21:59:22 +0200

    Tout le monde est ASCII Maintenant
    La préfecture a décidée que Gael c'était mieux

--- src/main/java/fr/fastandforum/filter/CharacterSetFilter.java
+++ src/main/java/fr/fastandforum/filter/CharacterSetFilter.java
@@ -7,13 +7,19 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 public class CharacterSetFilter implements Filter{
 	public void doFilter(
 		      ServletRequest request, 
 		      ServletResponse response, 
 		      FilterChain next) throws IOException, ServletException {
-		        request.setCharacterEncoding("UTF-8");
+				System.out.println("request: " + request.getCharacterEncoding() + " v " + request);
+				if(request instanceof HttpServletRequest)
+				{
+					System.out.println("URL: " +((HttpServletRequest)request).getRequestURL().toString());
+				}
+				request.setCharacterEncoding("UTF-8");
 		        response.setCharacterEncoding("UTF-8");
 		        next.doFilter(request, response);
 		    }
